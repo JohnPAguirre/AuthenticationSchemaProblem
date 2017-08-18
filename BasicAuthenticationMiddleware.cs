752e4c09a4b9cf032b24b0fa6183955cf5160580
John Aguirre <john.p.aguirre@norcrossvoip.com>
2017-08-17 20:17:28 -0400

    Not sure what I am missing to set the default ActiveAuthenticationSchemes to my authenticator

---
+++ BasicAuthenticationMiddleware.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+
+namespace AuthenticationSchemaProblem
+{
+    public class BasicAuthenticationMiddleware : AuthenticationHandler<AuthenticationSchemeOptions>
+    {
+        public BasicAuthenticationMiddleware(IOptionsMonitor<AuthenticationSchemeOptions> options,
+            ILoggerFactory logger, UrlEncoder encoder, ISystemClock clock)
+            : base(options, logger, encoder, clock)
+        {
+        }
+        protected override Task<AuthenticateResult> HandleAuthenticateAsync()
+        {
+            var principal = new GenericPrincipal(new GenericIdentity("User"), null);
+            var ticket = new AuthenticationTicket(principal, new AuthenticationProperties(), "BasicAuth");
+            return Task.FromResult(AuthenticateResult.Success(ticket));
+        }
+    }
+}
---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+
+namespace AuthenticationSchemaProblem.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        // GET api/values/Works
+        [HttpGet]
+        [Route("Works")]
+        [Authorize(ActiveAuthenticationSchemes = "BasicAuth")]
+        public string Works()
+        {
+            return "works";
+        }
+
+        // GET api/values/DoesNotWork
+        [HttpGet]
+        [Route("DoesNotWork")]
+        [Authorize]
+        public string DoesNotWork()
+        {
+            return "does not work";
+        }
+        
+    }
+}
